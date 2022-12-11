f52642ebdd29dd13cd3ec352169dc8569fe3af90
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2022-12-11 10:10:05 -0300

    First Microsservice

---
+++ S07_ErudioMicroservices.NET7-FirstMicrosservices/GeekShopping/GeekShopping.Api/Models/Context/MySqlContext.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Models.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext() { }
+
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
